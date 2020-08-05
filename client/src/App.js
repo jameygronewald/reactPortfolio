0e39173fa0313a68dc0e33ed47013b5b027c0afb
Jamey Gronewald <jrgronewald@gmail.com>
2020-08-04 18:17:19 -0700

    installed react router, created containers and wrote routes in app.js; confirmed app is serving up correct routes at endpoints

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import About from './containers/About/About';
+import Contact from './containers/Contact/Contact';
+import Portfolio from './containers/Portfolio/Portfolio';
+import NoMatch from './containers/NoMatch/NoMatch';
 
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={About} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/portfolio' component={Portfolio} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
   );
 }
 
